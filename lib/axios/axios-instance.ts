6b5640a08fff655b59f7623ce5a6a322d14516d6
Thitipong <thitipong.pathom@gmail.com>
2026-04-24 17:57:31 +0700

    revert default

--- lib/axios/axios-instance.ts
+++ lib/axios/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const customerApiBaseUrl = process.env.NEXT_PUBLIC_CUSTOMER_BASE_URL ?? 'https://customerservice-staging.up.railway.app'
+const customerApiBaseUrl = process.env.NEXT_PUBLIC_CUSTOMER_BASE_URL ?? 'http://localhost:8080'
 
 export const customerInstance = axios.create({
   baseURL: customerApiBaseUrl,
