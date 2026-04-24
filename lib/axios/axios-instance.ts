cc117e5fabedbe8912202fc12ea33f5da1f8b7bc
Thitipong <thitipong.pathom@gmail.com>
2026-04-24 17:00:05 +0700

    change env name

--- lib/axios/axios-instance.ts
+++ lib/axios/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const customerApiBaseUrl = process.env.CUSTOMER_BASE_URL ?? 'http://localhost:8080'
+const customerApiBaseUrl = process.env.NEXT_PUBLIC_CUSTOMER_BASE_URL ?? 'http://localhost:8080'
 
 export const customerInstance = axios.create({
   baseURL: customerApiBaseUrl,
